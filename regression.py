098cb2653dc461fb54d668b011925be419aeb84e
lbm-realty <labeebmuntasir@gmail.com>
2025-10-17 10:56:24 -0500

    Error analysis complete

--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+import numpy as np
 
 # Importing and reading the data
 teams = pd.read_csv("teams.csv")
@@ -38,6 +39,11 @@ test["predictions"] = predictions
 test.loc[test["predictions"] < 0, "predictions"] = 0
 test["predictions"] = test["predictions"].round()
 
+# Error analysis in depth
 error = mean_absolute_error(test["medals"], test["predictions"])
 errors = (test["medals"] - test["predictions"]).abs()
 error_by_team = errors.groupby(test["team"]).mean()
+medals_by_team = test["medals"].groupby(test["team"]).mean()
+error_ratio =  error_by_team / medals_by_team 
+error_ratio = error_ratio[np.isfinite(error_ratio)]
+error_ratio.plot.hist()
